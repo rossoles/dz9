a10b35857432d0ad91a36f98c2039d4d1ab531ee
unknown <rossol1@mail.ru>
2022-12-10 20:54:08 +0300

    dz9

---
+++ 64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значения M и N. Напишите рекурсивный метод, который выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void dz64(int n, int m)
+{
+    m++;
+    if(m % 3 == 0)
+        Console.Write(m + ", ");
+    if(m >= n)
+        return;
+    dz64(n, m);
+}
+dz64(n, m);
---
+++ 66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите рекурсивный метод, который найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void dz66(int n, int m, int summa = 0)
+{
+    summa += m;
+    m++;
+    if(m > n)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Сумма натуральных элементов в промежутке от M до N = " + summa);
+        Console.WriteLine();
+        return;
+    }
+    dz66(n, m, summa);
+}
+dz66(n, m);
+
+
+
+
